fbf895331bd1f1ab5f25ccb779def5ebc85577a4
yangjin <kingyouge@gmail.com>
2019-09-01 01:48:01 +0800

    modify: modify config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,16 @@
 module.exports = {
   title: 'Hello VuePress',
-  description: 'Just playing around'
+  description: 'Just playing around',
+
+  // webpack alias
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@alias': 'path/to/some/dir'
+      }
+    }
+  },
+
+  // base path
+  base: '/docs/'
 }
